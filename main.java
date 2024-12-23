e4717fc7b76255461ec5399be2fe7492bd7dd84e
IInball <tzafrinbal@gmail.com>
2024-12-22 18:00:51 -0800

    Drawing Hand In

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class ScanStuff{
+    public static void main(String args[]) {
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------Computer Programming 11 - VLN-----------------------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("  oooo   oooo             oooo   oooo             oooo   oooo             oooo   oooo             oooo   oooo             oooo   oooo             oooo   oooo             ooo");
+        System.out.println(" o    o o    o           o    o o    o           o    o o    o           o    o o    o           o    o o    o           o    o o    o           o    o o    o           o   ");
+        System.out.println("o      o      o         o      o      o         o      o      o         o      o      o         o      o      o         o      o      o         o      o      o         o    ");
+        System.out.println(" o           o           o           o           o           o           o           o           o           o           o           o           o           o           o   ");
+        System.out.println("  o         o             o         o             o         o             o         o             o         o             o         o             o         o             o  ");
+        System.out.println("    o      o               o      o                o      o                o      o                o      o                o      o                o      o                o ");
+        System.out.println("     o  o                    o  o                    o  o                    o  o                    o  o                    o  o                    o  o                    ");
+        System.out.println("      o                       o                       o                       o                       o                       o                       o                      ");
+        System.out.println("---------------------------------------------------------------------------------Inbal Tzafrir-------------------------------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("----------------------------------------------------------------------------------22-12-2024---------------------------------------------------------------------------------");
+
+    }
+}
